70241759905e89439d443d0a77a2aa98e52f6d10
lz233 <lz23333@outlook.com>
2021-11-28 00:41:49 +0800

    import danmaku library update to kotlin 1.5.31

---
+++ danmaku/src/main/java/com/kd/easybarrage/Barrage.java
@@ -0,0 +1,66 @@
+package com.kd.easybarrage;
+
+import android.graphics.Color;
+
+public class Barrage {
+
+    private String content;
+    private int color;
+    private boolean showBorder;
+    private int backGroundColor;
+
+    public Barrage(String content) {
+        this(content, android.R.color.black, false, Color.WHITE);
+    }
+
+    public Barrage(String content, int color) {
+        this(content, color, false, Color.WHITE);
+    }
+
+    public Barrage(String content, boolean showBorder) {
+        this(content, android.R.color.black, showBorder, Color.WHITE);
+    }
+
+    public Barrage(String content, int color, int backGroundColor) {
+        this(content, color, false, backGroundColor);
+    }
+
+    private Barrage(String content, int color, boolean showBorder, int backGroundColor) {
+        this.content = content;
+        this.color = color;
+        this.showBorder = showBorder;
+        this.backGroundColor = backGroundColor;
+    }
+
+    public boolean isShowBorder() {
+        return showBorder;
+    }
+
+    public void setShowBorder(boolean showBorder) {
+        this.showBorder = showBorder;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getBackGroundColor() {
+        return backGroundColor;
+    }
+
+    public void setBackGroundColor(int backGroundColor) {
+        this.backGroundColor = backGroundColor;
+    }
+}
